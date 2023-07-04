0449ca0a7a47a1cd98e91b86d9dc843223df2eb3
ArturPedziwiatr <ArturPedziwiatrProjekty@gmail.com>
2023-07-04 20:20:43 +0200

    .

--- src/https/controller/celantureController.js
+++ src/https/controller/celantureController.js
@@ -1,6 +1,8 @@
-import fetch from 'node-fetch';
-import { Keys } from '../../bootstrap/keys.js';
-import { container } from '../../event/Inversify.js';
+import fetch from 'node-fetch'
+import path from 'path'
+import fs from 'fs'
+import { Keys } from '../../bootstrap/keys.js'
+import { container } from '../../event/Inversify.js'
 
 export class CelantureController {
   constructor(appconfig = container.get(Keys.Config)) {
@@ -9,12 +11,36 @@ export class CelantureController {
 
   async postFile(req, res) {
     try {
-      console.log('asdfasdf');
-      const data = req.redirect(path.join(this.appConfig.getCelanturURL(), 'file'))
-      console.log(data)
-      res.send('oki')
-    } catch(err) {
-      console.error(err);
+      // const { headers, query, auth } = req
+
+      console.log(`${this.appconfig.getCelanturURL()}${req.path}`);
+      // const data = await fetch(
+      //   `${this.appconfig.getCelanturURL()}${req.path}`,
+      //   {
+      //     method: 'GET',
+      //     headers,
+      //     auth
+      //   }
+      // )
+      // const xd = await data.arrayBuffer()
+      // console.log(xd)
+      // const xdasd = fs.writeFileSync('./asd.jpg',Buffer.from(xd))
+      res.send('xdasd')
+    } catch {
+      res.send('sad')
     }
+  }
+
+  async getAllFiles(req, res) {
+    const { headers, auth, query } = req
+    fetch(`${this.appconfig.getCelanturURL()}/list`, {
+      method: 'get',
+      headers,
+      query,
+      auth,
+    })
+      .then(async (res) => await res.text())
+      .then((json) => res.send(json))
+      .catch((err) => res.send(err))
   }
 }
